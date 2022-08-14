06aeba1aa2736415ea54a4589334706a74c35d21
Latone <zhuchkov_01@mail.ru>
2022-08-14 21:53:17 +0400

    Добавьте файлы проекта.

---
+++ Poverka_TCP_Listener.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poverka_Service
+{
+    public partial class Poverka_TCP_Listener : ServiceBase
+    {
+        EventLog eventLog1;
+        public Poverka_TCP_Listener()
+        {
+            InitializeComponent();
+            eventLog1 = new EventLog();
+
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "MySource", "MyNewLog");
+            }
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poverka_Service
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Poverka_TCP_Listener()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
